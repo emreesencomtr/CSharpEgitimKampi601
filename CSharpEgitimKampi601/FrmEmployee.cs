aff693aaeea05da35eaf15907d89b3132bb1e6ff
Emre Esen <emreakkaya1905@gmail.com>
2025-01-12 21:56:33 +0300

    C# Eğitim Kampı Ders 27

---
+++ CSharpEgitimKampi601/FrmEmployee.cs
@@ -0,0 +1,82 @@
+﻿using MongoDB.Driver;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi601
+{
+    public partial class FrmEmployee : Form
+    {
+        public FrmEmployee()
+        {
+            InitializeComponent();
+        }
+        string connectionString = "Server= localhost; port=5432; Database=CustomerDb; user Id= postgres; password=13587410";
+        void EmployeeList()
+        {
+            var connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+            string query = "Select * From Employees";
+            var command = new NpgsqlCommand(query, connection);
+            var adapter = new NpgsqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            connection.Close();
+
+        }
+
+        void DepartmenList()
+        {
+            var connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+            string query = "Select * From Departments";
+            var command = new NpgsqlCommand(query, connection);
+            var adapter = new NpgsqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            cmbEmployeeDepartment.DisplayMember = "DepartmentName";
+            cmbEmployeeDepartment.ValueMember = "DepartmentId";
+            cmbEmployeeDepartment.DataSource= dt;
+            connection.Close();
+        }
+        private void btlnList_Click(object sender, EventArgs e)
+        {
+            EmployeeList();
+        }
+
+        private void FrmEmployee_Load(object sender, EventArgs e)
+        {
+            EmployeeList();
+            DepartmenList();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            string emloyeeName= txtEmployeeName.Text;
+            string employeeSurname = txtEmployeeSurname.Text;
+            decimal employeeSalaray = decimal.Parse(txtEmployeeSalary.Text);
+            int departmentId = int.Parse(cmbEmployeeDepartment.SelectedValue.ToString());
+
+            var connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+            string query = "insert into Employees (EmployeeName,EmployeeSurname,EmployeeSalaray,Departmentid) values (@employeeName, @employeeSurname, @employeeSalaray, @departmentid)";
+            var command = new NpgsqlCommand(@query, connection);
+            command.Parameters.AddWithValue("@employeeName", emloyeeName);
+            command.Parameters.AddWithValue("@employeeSurname", employeeSurname);
+            command.Parameters.AddWithValue("@employeeSalaray", employeeSalaray);
+            command.Parameters.AddWithValue("@departmentid", departmentId );
+            command.ExecuteNonQuery();
+            MessageBox.Show("Ekleme İşlemi Başarılı","Bilgi",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            connection.Close();
+            EmployeeList();
+        }
+    }
+}
